aaff8054d958fa8880cc293b30e815d41d904285
Vasiliy Gromkov <v.gromkov2014@yandex.ru>
2022-10-16 14:40:30 +0500

    add autoprefix, app description

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,9 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const openBtn = document.querySelector(".models-list-open")
     const modelsMenu = document.querySelector(".models-list")
+    const body = document.querySelector("body")
 
     openBtn.addEventListener("click", () => {
         modelsMenu.classList.toggle("open")
         openBtn.classList.toggle("active")
+        body.classList.toggle("hidden")
     })
 })
